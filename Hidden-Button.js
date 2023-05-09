491d1a14adb422b1e84867d79af0cb789d6e4d3f
Vlad Focuta <132595962+VladFocuta@users.noreply.github.com>
2023-05-09 18:17:25 +0200

    Update Hidden-Button.js

--- Hidden-Button.js
+++ Hidden-Button.js
@@ -1,27 +1,21 @@
-let button1 = document.getElementById("button1");
-let button2 = document.getElementById("button2");
-let button3 = document.getElementById("button3");
-let buttonGenerator = document.getElementById("buttonGener");
+let buttons = document.querySelectorAll("button");
 let randomNumber;
 
 function gnrRndNmbTill3() {
    randomNumber = Math.floor(Math.random() * 3) + 1;
 }
 
-let randomNumberGenerator, buttons_amount, three = 3;
+let randomNumberGenerator, buttons_amount, count = 3;
 
 function user_input() {
    buttons_amount = prompt("Enter the amount of buttons that you need");
    if (buttons_amount != null) {
-      let guessed_buttons = [];
       let buttonsContainer = document.getElementById("buttonsContainer");
       for (let i = 1; i <= buttons_amount; i++) {
          let button = document.createElement("button");
-         ++three;
-         button.id = "button" + three;
-         button.innerHTML = "Button " + three;
+         button.id = "button" + ++count;
+         button.innerHTML = "Button " + count;
          button.onclick = winner_or_not;
-         guessed_buttons.push(button);
          buttonsContainer.appendChild(button);
       }
       randomNumberGenerator = Math.floor(Math.random() * buttons_amount) + 4;
@@ -31,14 +25,9 @@ function user_input() {
 function winner_or_not(e) {
    let clickedButton = e.target;
    let clickedButtonNumber = parseInt(clickedButton.id.replace("button", ""));
-   if (clickedButtonNumber === randomNumber && clickedButtonNumber <= 3) {
+   if ((clickedButtonNumber === randomNumber && clickedButtonNumber <= 3) || (clickedButtonNumber > 3 && clickedButtonNumber === randomNumberGenerator)) {
       alert("castigator");
-   } else if (clickedButtonNumber !== randomNumber && clickedButtonNumber <= 3) {
-      alert("necastigator");
-   }
-   if (clickedButtonNumber === randomNumberGenerator && clickedButtonNumber > 3) {
-      alert("castigator");
-   } else if (clickedButtonNumber !== randomNumberGenerator && clickedButtonNumber > 3) {
+   } else {
       alert("necastigator");
    }
 }
