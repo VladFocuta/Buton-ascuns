3c6bdd63368ddb3eb997b1e0dcd2c99114c0fed3
Vlad Focuta <132595962+VladFocuta@users.noreply.github.com>
2023-05-11 18:25:42 +0200

    Update Hidden-Button.js

--- Hidden-Button.js
+++ Hidden-Button.js
@@ -1,30 +1,30 @@
 let buttons = document.querySelectorAll("button");
 let randomNumber;
 
-function gnrRndNmbTill3() {
+function genRandomNumber() {
    randomNumber = Math.floor(Math.random() * 3) + 1;
 }
 
-let randomNumberGenerator, buttons_amount, count = 3;
+let buttonsAmount, count = 3;
 
-function user_input() {
-   buttons_amount = prompt("Enter the amount of buttons that you need");
-   if (buttons_amount != null) {
+function getButtons() {
+   buttonsAmount = prompt("Enter the amount of buttons that you need");
+   if (buttonsAmount != null) {
       let buttonsContainer = document.getElementById("buttonsContainer");
-      for (let i = 1; i <= buttons_amount; i++) {
+      for (let i = 1; i <= buttonsAmount; i++) {
          let button = document.createElement("button");
          button.id = "button" + ++count;
          button.innerHTML = "Button " + count;
-         button.onclick = winner_or_not;
+         button.onclick = winnerOrNot;
          buttonsContainer.appendChild(button);
       }
-      if (buttons_amount > 0) {
-         randomNumber = Math.floor(Math.random() * buttons_amount) + 4;
+      if (buttonsAmount > 0) {
+         randomNumber = Math.floor(Math.random() * buttonsAmount) + 4;
       }
    }
 }
 
-function winner_or_not(e) {
+function winnerOrNot(e) {
    let clickedButton = e.target;
    let clickedButtonNumber = parseInt(clickedButton.id.replace("button", ""));
    if (clickedButtonNumber === randomNumber) {
@@ -33,4 +33,4 @@ function winner_or_not(e) {
       alert("necastigator");
    }
 }
-window.onload = gnrRndNmbTill3;
+window.onload = genRandomNumber;
